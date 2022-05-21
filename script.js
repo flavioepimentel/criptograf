9527d84f53fc6dbd1eb4cdf60fa9a605d1d35985
flavioepimentel <flavio.ericksan@hotmail.com>
2022-05-21 14:27:47 -0300

    Implementa as funções

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let output = document.getElementById("output");
 let textarea = document.getElementById("textarea");
 
-document.getElementById('criptografar').addEventListener('click', criptografer(textarea.value))
-document.getElementById('descriptografer').addEventeListiner('click',descriptografer(textarea.value))
-
-let img = document.querySelector('.imagem-texto')
-
-// document.getElementById('descriptografer').onclick 
 const matrizCript = [
   ["a", "ai"],
   ["e", "enter"],
@@ -15,39 +9,45 @@ const matrizCript = [
   ["u", "ufat"],
 ];
 
-function criptografer(texto) {
-    const frase = texto.toLowerCase()
-    let string = ''
-    for (let i = 0; i < matrizCript.length; i++){
-        if(frase.includes(matrizCript[i][0])){
-            string =  frase.split(matrizCript[i][0]).join(matrizCript[i][1])
-        }
+function criptograferFunction() {
+  let texto = textarea.value;
+  const frase = texto.toLowerCase();
+  let string = "";
+
+  for (let i = 0; i < matrizCript.length; i++) {
+    if (frase.includes(matrizCript[i][0])) {
+      string = frase.split(matrizCript[i][0]).join(matrizCript[i][1]);
     }
-    console.log(string);
+  }
+
+  document.getElementById("imagem-texto").style.visibility = "hidden";
+  document.getElementById("copy").style.visibility = "visible";
+  document.getElementById("output").innerHTML = string;
 }
 
-function descriptografer(texto) {
-    const frase = texto.toLowerCase()
-    let string = ''
-    for (let i = 0; i < matrizCript.length; i++){
-        if(frase.includes(matrizCript[i][0])){
-            string =  frase.split(matrizCript[i][1]).join(matrizCript[i][0])
-        }
+function descriptograferFunction() {
+  let texto = textarea.value;
+  const frase = texto.toLowerCase();
+  let string = "";
+
+  for (let i = 0; i < matrizCript.length; i++) {
+    if (frase.includes(matrizCript[i][0])) {
+      string = frase.split(matrizCript[i][1]).join(matrizCript[i][0]);
     }
-    console.log(string);
+  }
+
+  document.getElementById("imagem-texto").style.visibility = "hidden";
+  document.getElementById("copy").style.visibility = "visible";
+  document.getElementById("output").innerHTML = string;
 }
 
 function controlC() {
-    /* Capturando o campo do texto */
-    var copyText = document.getElementById("output");
-  
-    /* Selecionando o texto */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copiando o texto */
-    navigator.clipboard.writeText(copyText.value);
-  
-    /* Alerta informando que o texto foi copiado */
-    alert("Copied the text: " + copyText.value);
-  }
+  /* Capturando o campo do texto */
+  let copyText = document.getElementById("output");
+
+  /* Copiando o texto */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alerta informando que o texto foi copiado */
+  alert("Text copied");
+}
